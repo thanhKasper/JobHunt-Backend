48e66fcba05528bb9b67335d0a05e805fcb541a7
Kieu Tien Thanh <kieutienthanh207@gmail.com>
2025-08-14 21:04:37 +0700

    create tests for job analysis feature

---
+++ tests/JobHunt.ServiceTests/JobPostAnalysisTests/CompanySynchronizerTest.cs
@@ -0,0 +1,65 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using JobHunt.Core.Domain.Entities;
+using JobHunt.Core.Domain.RepositoryContracts;
+using JobHunt.Core.Services.JobAnalysisUseCase.CompanySynchronizer;
+using JobHunt.Core.Services.JobAnalysisUseCase.JobPostingAnalyzer.DTO;
+using Moq;
+
+namespace JobHunt.ServiceTests.JobPostAnalysisTests
+{
+    public class CompanySynchronizerTest
+    {
+        private readonly CompanySynchronizerImp _companySynchronizer;
+        private readonly Mock<ICompanyGetRepository> _companyGetRepoMock;
+        private readonly Mock<ICompanyUpdateRepository> _companyUpdateRepoMock;
+        private readonly Fixture _fixture;
+        
+        public CompanySynchronizerTest()
+        {
+            _companyGetRepoMock = new Mock<ICompanyGetRepository>();
+            _companyUpdateRepoMock = new Mock<ICompanyUpdateRepository>();
+            _fixture = new Fixture();
+
+            _companySynchronizer = new CompanySynchronizerImp(
+                _companyGetRepoMock.Object,
+                _companyUpdateRepoMock.Object
+            );
+        }
+
+        [Fact]
+        public void SynchronizeCompanyFromJobPost_WhenNormal_ShouldReturnResult()
+        {
+            FoundCompanyInTheDataSource();
+            SetupCompanyUpToDate();
+
+            var jobPost = GetJobPost();
+            var expected = _companySynchronizer.SynchronizeCompanyFromJobPost(jobPost);
+
+            expected.Should().NotBeNull();
+        }
+
+        private void FoundCompanyInTheDataSource()
+        {
+            Company company = _fixture.Create<Company>();
+            _companyGetRepoMock.Setup(mock => mock.FindByNameAsync(It.IsAny<string>()))
+                .ReturnsAsync(company);
+        }
+
+        private void SetupCompanyUpToDate()
+        {
+            Company company = _fixture.Create<Company>();
+            _companyGetRepoMock.Setup(mock => mock.FindByNameAsync(It.IsAny<string>()))
+                .ReturnsAsync(company);
+            _companyGetRepoMock.Setup(mock => mock.FindByWebsiteAsync(It.IsAny<string>()))
+                .ReturnsAsync(company);
+            _companyGetRepoMock.Setup(mock => mock.FindByAddressAsync(It.IsAny<string>()))
+                .ReturnsAsync(company);
+        }
+
+        private JobPosting GetJobPost()
+        {
+            return _fixture.Create<JobPosting>();
+        }
+    }
+}
---
+++ tests/JobHunt.ServiceTests/JobPostAnalysisTests/JobPostNormalizationTest.cs
@@ -0,0 +1,51 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using JobHunt.Core.Domain.Entities;
+using JobHunt.Core.Domain.RepositoryContracts;
+using JobHunt.Core.Services.JobAnalysisUseCase.CompanySynchronizer;
+using JobHunt.Core.Services.JobAnalysisUseCase.JobPostingAnalyzer.DTO;
+using JobHunt.Core.Services.JobAnalysisUseCase.JobPostingNormalizer;
+using JobHunt.Core.Services.JobAnalysisUseCase.JobPostingNormalizer.Interface;
+using Moq;
+
+namespace JobHunt.ServiceTests.JobPostAnalysisTests
+{
+    public class JobPostNormalizationTest
+    {
+        private readonly IJobPostNormalizer _jobPostNormalizer;
+        private readonly Mock<ICompanySynchronizer> _companySynchronizerMock;
+        private readonly Fixture _fixture;
+
+        public JobPostNormalizationTest()
+        {
+            _fixture = new Fixture();
+
+            _companySynchronizerMock = new Mock<ICompanySynchronizer>();
+            _jobPostNormalizer = new JobPostNormalizer(
+                _companySynchronizerMock.Object
+            );
+        }
+
+        [Fact]
+        public void NormalizeJobPost_WhenNormalizeJobPosting_ShouldReturnJob()
+        {
+            var jobPost = CreateJobPosting();
+            LinkCompanyFromJobPost(jobPost);
+            var result = _jobPostNormalizer.NormalizeJobPost(jobPost);
+
+            result.Should().NotBeNull();
+            result.Company.Should().NotBeNull();
+        }
+
+        private JobPosting CreateJobPosting()
+        {
+            return _fixture.Create<JobPosting>();
+        }
+
+        private void LinkCompanyFromJobPost(JobPosting jobPost)
+        {
+            _companySynchronizerMock.Setup(mock => mock.SynchronizeCompanyFromJobPost(jobPost))
+                .Returns(_fixture.Create<Company>());
+        }
+    }
+}
